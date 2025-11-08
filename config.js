923109202d5491ee2d91ec66e77687129f013176
ctlpowercmr-tech <ctlpowercmr@gmail.com>
2025-11-09 00:50:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Configuration de l'API - À MODIFIER AVEC VOTRE URL RENDER
 const CONFIG = {
-  API_URL: 'https://ns-rzdd.onrender.com' // Remplacez par votre URL Render
+  API_URL: 'https://ns-1-0qzl.onrender.com' // Remplacez par votre URL Render
 };
